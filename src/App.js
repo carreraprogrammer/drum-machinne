2219804669080d5e1c144ee2ba082f850b33c78f
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-04 22:58:06 -0500

    Delet unused variable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import ControlPanel from "./Components/ControlPanel";
 import Pad from "./Components/Pad";
 import './Saas/App.scss';
 import drumKeys from './Resources/bankOne'
-import drumKeysTwo from "./Resources/bankTwo";
 import { useState } from 'react';
 
 const App = () => {
